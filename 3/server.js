4974c0905cdfc16cbcc0d75fdac6fec2ad71e3b4
yoel <yoel.palermo@davinci.edu.ar>
2024-09-01 20:57:04 -0300

    configserver

--- 3/server.js
+++ 3/server.js
@@ -1,1 +1,14 @@
-console.log("asd");
+// console.log("asd");
+
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end("hello world");
+})
+
+server.listen(PORT, () => {
+    console.log(`server running on port: http://localhost:${PORT}`)
+})
