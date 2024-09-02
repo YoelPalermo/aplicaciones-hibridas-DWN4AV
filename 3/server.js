58a1c2164d333f900bd3dd452a5b3d306041a35a
yoel <yoel.palermo@davinci.edu.ar>
2024-09-01 21:18:43 -0300

    datosAlumno

--- 3/server.js
+++ 3/server.js
@@ -5,8 +5,28 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end("hello world");
+
+    const url = req.url;
+    const method = req.method;
+    
+    if(url === "/" && method === 'GET'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("hello world");
+    }else if (url === "/alumno" && method === 'GET'){
+        const data={
+            nombre: "Yoel Palermo",
+            comision: "DWN4AV"
+        }
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(data));
+    }
+
+
+
+
+
+// res.writeHead(200, {'Content-Type': 'text/plain'});
+// res.end("hello world");
 })
 
 server.listen(PORT, () => {
